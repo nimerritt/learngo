7e480f811260d81fff1b1586a6958d2a210cd613
Nicholas Merritt <nimerritt@gmail.com>
2016-06-02 10:09:59 +0200

    Implement recursive in-order tree walk function, Walk
    Need two functions to ensure close is only called once outside of the
    recursive calls.

--- trees/trees.go
+++ trees/trees.go
@@ -3,8 +3,42 @@ package main
 import "fmt"
 import "golang.org/x/tour/tree"
 
+// Walk the tree in-order
+// (left subtree, node, right subtree)
+// and write the values to the channel
+
+func walk(t *tree.Tree, c chan int) {
+	// base case
+	if t == nil {
+
+	}
+
+	// recursive step
+	if t.Left != nil {
+		walk(t.Left, c)
+	}
+
+	c <- t.Value
+
+	if t.Right != nil {
+		walk(t.Right, c)
+	}
+}
+
+func Walk(t *tree.Tree, c chan int) {
+	walk(t, c)
+	close(c)
+}
+
+func dump(c chan int) {
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
 func main() {
 	var tree = tree.New(1)
-	fmt.Println(tree)
-
+	var out = make(chan int, 10)
+	go Walk(tree, out)
+	dump(out)
 }
